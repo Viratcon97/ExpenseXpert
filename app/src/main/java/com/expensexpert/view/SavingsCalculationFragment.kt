fa696ae70240c307151ee21bc355825c6157defa
Virat Contractor <viratcontractor97@gmail.com>
2024-05-20 17:46:38 -0400

    Savings Calculation UI changes.

--- app/src/main/java/com/expensexpert/view/SavingsCalculationFragment.kt
+++ app/src/main/java/com/expensexpert/view/SavingsCalculationFragment.kt
@@ -6,18 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.expensexpert.R
+import com.expensexpert.databinding.FragmentHistoryBinding
+import com.expensexpert.databinding.FragmentSavingsCalculationBinding
+import com.expensexpert.viewmodel.HistoryViewModel
 
 
 class SavingsCalculationFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private lateinit var savingsCalculationBinding: FragmentSavingsCalculationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_savings_calculation, container, false)
+        savingsCalculationBinding = FragmentSavingsCalculationBinding.inflate(layoutInflater)
+        return savingsCalculationBinding.root
     }
 }
